f42729397a7dab5b6e4d946be054a703cd0ad12e
lcw567 <1lcw04181@naver.com>
2024-11-01 17:35:48 +0900

    커뮤니티 ui 추가

--- src/main/java/com/cs/workdream/board/controller/BoardController.java
+++ src/main/java/com/cs/workdream/board/controller/BoardController.java
@@ -6,8 +6,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class BoardController {
 
-    @GetMapping("/communityHome")
+	@GetMapping("/communityHome")
     public String showCommunityHome() {
-        return "board/communityHome";
+        return "board/communityHome"; // communityHome.jsp
     }
-}  
+
+    @GetMapping("/communityPost")
+    public String showCommunityPost() {
+        return "board/communityPost"; // communityPost.jsp
+    }
+
+    @GetMapping("/communityView")
+    public String showCommunityWrite() {
+        return "board/communityView"; // communityView.jsp
+    }
+
+    @GetMapping("/communityList")
+    public String showCommunityList() {
+        return "board/communityList"; // communityList.jsp
+    }
+}
