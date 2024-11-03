966ded3fc9397d41cf58991f79463459c50fcd07
ajy3753 <ajy3753@gmail.com>
2024-11-03 22:08:04 +0900

    log4j 오류 수정 및 안내 추가

--- src/main/java/com/cs/workdream/aspect/LogginAOP.java
+++ src/main/java/com/cs/workdream/aspect/LogginAOP.java
@@ -15,11 +15,18 @@ import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Aspect
 @Component
-@Slf4j
 public class LogginAOP {
-
+	// [ log cannot be resolbed ] 에러 해결법
+	// 프로젝트에 포함된 Maven Dependaencies > lombok.jar 찾기 (Maven 3.9.9 기준 lombok-1.18.34.jar로 되어있음)
+	// 우클릭 > Run As > Java Application
+	// 인스톨 실행 > 설치 경로 설정 > Spring이 깔려있는 폴더의 STS.ini 파일 선택. (ex. sts-3.9.13.RELEASE/STS.ini)
+	// 선택 후 STS와 이클립스 두 가지가 경로에 나타난다면 정상적으로 선택된 겁니다. (이클립스가 뜨지 않으면 따로 이클립스 설치 폴더 > ini 파일 추가)
+	// 인스톨이 완료되면 Project > Clean 또는 Maven > Update Project 실행 (순서대로 둘 다 하는 것을 추천)
+	// ※ 해당 오류는 소스를 새로 받을 때마다 랜덤하게 일어날 수 있으므로 해결 방법을 꼭 숙지해두세요.
+	
 	@Pointcut("execution(* com.cs.workdream..controller.*.*(..) )")
 	private void cut() {}
 	
