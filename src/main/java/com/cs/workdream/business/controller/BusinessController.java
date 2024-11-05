081b301a4923f36f64304065a16b3d508c7cbaf0
ajy3753 <ajy3753@gmail.com>
2024-11-05 14:49:13 +0900

    구직자 즐겨찾기 페이지 추가

--- src/main/java/com/cs/workdream/business/controller/BusinessController.java
+++ src/main/java/com/cs/workdream/business/controller/BusinessController.java
@@ -21,4 +21,9 @@ public class BusinessController {
     public String appList() {
         return "business/applicantsList";
     }
+ 
+    @GetMapping("/business/bookmark")
+    public String bookmarkList() {
+        return "business/bookmarkList";
+    }
 }
