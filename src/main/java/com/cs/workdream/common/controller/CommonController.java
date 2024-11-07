b8c4897d959744ec8159f4cec20072db09b526a0
ajy3753 <ajy3753@gmail.com>
2024-11-07 17:40:00 +0900

    로그인 페이지 추가

--- src/main/java/com/cs/workdream/common/controller/CommonController.java
+++ src/main/java/com/cs/workdream/common/controller/CommonController.java
@@ -4,9 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class CommonController {
     @GetMapping("/index")
     public String index() {
         return "index";
+    }
+    
+    @GetMapping("/login")
+    public String login() {
+        return "common/login";
     }
 }
