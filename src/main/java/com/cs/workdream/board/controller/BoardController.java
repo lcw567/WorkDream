4d2857c9b912e9cb88ac8e575fb88017834c6755
seojieu <dasiboja01@gmail.com>
2024-11-15 17:49:57 +0900

    공고 목록 js 수정

--- src/main/java/com/cs/workdream/board/controller/BoardController.java
+++ src/main/java/com/cs/workdream/board/controller/BoardController.java
@@ -27,4 +27,10 @@ public class BoardController {
     public String showCommunityList() {
         return "board/communityList"; // communityList.jsp
     }
+    
+    @GetMapping("/listOfJobOpening")
+    public String showjobOpeningList() {
+        return "board/listOfJobOpening"; // listOfJobOpening.jsp
+    }
+    
 }
