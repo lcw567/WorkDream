3743cf7fa571927a80b7ebf02db2f168b8000ccb
cndgur <lee607080@naver.com>
2024-11-17 02:11:22 +0900

    이력서 부분 수정

--- src/main/java/com/cs/workdream/resume/model/vo/Experience.java
+++ src/main/java/com/cs/workdream/resume/model/vo/Experience.java
@@ -1,28 +1,33 @@
 package com.cs.workdream.resume.model.vo;
 
-import java.sql.Date;
+import java.time.LocalDate;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import lombok.ToString;
+
 @Data
-@Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
 @EqualsAndHashCode
 public class Experience {
-	private Integer experience_no;
-	private Integer resume_no;
-	private int person_no;
-	private String activity_type;     // 활동구분
-    private String organization_name; // 기관/장소명
-    private Date start_date;          // 시작년월
-    private Date end_date;            // 종료년월
-    private String description;       // 활동설명
+    private Integer experienceNo;
+    private Integer resumeNo;
+    private int personNo;
+    private String activityType;
+    private String organizationName;
+
+    @DateTimeFormat(pattern = "yyyy-MM")
+    private LocalDate startDate;
+
+    @DateTimeFormat(pattern = "yyyy-MM")
+    private LocalDate endDate;
+
+    private String description;
 }
+
--- src/main/java/com/cs/workdream/resume/service/ResumeService.java
+++ src/main/java/com/cs/workdream/resume/service/ResumeService.java
@@ -2,6 +2,18 @@ package com.cs.workdream.resume.service;
 
 import com.cs.workdream.resume.model.vo.Resume;
 
+/**
+ * ResumeService 인터페이스는 이력서 관련 비즈니스 로직을 정의합니다.
+ */
 public interface ResumeService {
-	int saveResume(Resume resume);
+    
+    /**
+     * 이력서를 저장하는 메서드
+     *
+     * @param resume 저장할 Resume 객체
+     * @return 저장 성공 시 1, 실패 시 0
+     */
+    int saveResume(Resume resume);
+    
+    // 필요에 따라 추가적인 메서드를 정의할 수 있습니다.
 }
