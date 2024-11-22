e0d942cca3755f3e241e5c7885c8b6c6c551ad20
cndgur <lee607080@naver.com>
2024-11-22 09:42:05 +0900

    resume serviceimpl 수정

--- src/main/java/com/cs/workdream/resume/service/ResumeServiceImpl.java
+++ src/main/java/com/cs/workdream/resume/service/ResumeServiceImpl.java
@@ -15,7 +15,6 @@ import com.cs.workdream.resume.model.vo.Resume;
 import com.cs.workdream.resume.model.vo.BasicInfo;
 import com.cs.workdream.resume.model.vo.Skill;
 import com.cs.workdream.resume.model.vo.Career;
-import com.cs.workdream.resume.model.vo.Award;
 
 import java.io.File;
 import java.io.IOException;
@@ -51,89 +50,14 @@ public class ResumeServiceImpl implements ResumeService {
      */
     @Override
     @Transactional
-    public boolean saveResume(Resume resume, MultipartFile userPic) {
-        try {
-            // 프로필 이미지 처리
-            if (userPic != null && !userPic.isEmpty()) {
-                // 원본 파일 이름
-                String originalFilename = userPic.getOriginalFilename();
-                // 고유한 파일 이름 생성
-                String uniqueFilename = UUID.randomUUID().toString() + "_" + originalFilename;
-                // 파일을 저장할 경로
-                String filePath = uploadDir + File.separator + uniqueFilename;
-                File dest = new File(filePath);
-                // 디렉토리가 존재하지 않으면 생성
-                if (!dest.getParentFile().exists()) {
-                    dest.getParentFile().mkdirs();
-                }
-                // 파일 저장
-                userPic.transferTo(dest);
-                // Resume 객체에 파일 이름 설정
-                resume.setUserPic(uniqueFilename);
-            }
+	public boolean saveResume(Resume resume) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
-            // 이력서 기본 정보 저장
-            resumeDao.insertResume(resume);
-            
-            Integer resumeId = resume.getResumeNo(); // MyBatis가 자동 생성한 ID를 설정해야 함
-            logger.info("Resume saved with ID: {}", resumeId);
-
-            // 학력 정보 저장
-            if (resume.getEducations() != null) {
-                for (AcademicAbility edu : resume.getEducations()) {
-                    edu.setResumeNo(resumeId);
-                    resumeDao.insertEducation(edu);
-                }
-            }
-
-            // 경력 정보 저장
-            if (resume.getCareers() != null) {
-                for (Career career : resume.getCareers()) {
-                    career.setResumeNo(resumeId);
-                    resumeDao.insertCareer(career);
-                }
-            }
-
-            // 스킬 저장
-            if (resume.getSkills() != null) {
-                for (Skill skill : resume.getSkills()) {
-                    skill.setResumeNo(resumeId);
-                    resumeDao.insertSkill(skill);
-                }
-            }
-
-            // 자격증 저장
-            if (resume.getQualifications() != null) {
-                for (Qualification qual : resume.getQualifications()) {
-                    qual.setResumeNo(resumeId);
-                    resumeDao.insertQualification(qual);
-                }
-            }
-
-            // 수상내역 저장
-            if (resume.getAwards() != null) {
-                for (Award award : resume.getAwards()) {
-                    award.setResumeId(resumeId);
-                    resumeDao.insertAward(award);
-                }
-            }
-
-            // 취업 우대 사항 저장
-            EmploymentPreferences preferences = resume.getEmploymentPreferences();
-            if (preferences != null) {
-                preferences.setResumeNo(resumeId);
-                resumeDao.insertEmploymentPreferences(preferences);
-            }
-
-            return true;
-        } catch (IOException e) {
-            logger.error("파일 업로드 중 오류 발생: ", e);
-            // 트랜잭션 롤백
-            throw new RuntimeException("파일 업로드 실패", e);
-        } catch (Exception e) {
-            logger.error("이력서 저장 중 오류 발생: ", e);
-            // 트랜잭션 롤백
-            throw new RuntimeException("이력서 저장 실패", e);
-        }
-    }
+	@Override
+	public boolean saveResume(Resume resume, MultipartFile userPic) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
