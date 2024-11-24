ae52ca78d9c3a044855346a4f7fdb757de8e7bd8
lcw567 <1lcw04181@naver.com>
2024-11-24 16:44:30 +0900

    기업정보 수정

--- src/main/java/com/cs/workdream/mypage/controller/MyPageController.java
+++ src/main/java/com/cs/workdream/mypage/controller/MyPageController.java
@@ -1,28 +1,47 @@
 package com.cs.workdream.mypage.controller;
 
-import com.cs.workdream.member.model.vo.Member;
-import com.cs.workdream.usertags.model.vo.UserTag;
-import com.cs.workdream.usertags.service.UserTagsService;
+import java.util.Collections;
+import java.util.List;
+
+// 필요한 import 추가
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.http.HttpSession;
-import java.util.Collections;
-import java.util.List;
+import com.cs.workdream.business.model.vo.Business;
+import com.cs.workdream.business.service.BusinessProfileService;
+import com.cs.workdream.member.model.vo.Member;
+import com.cs.workdream.usertags.model.vo.UserTag;
+import com.cs.workdream.usertags.service.UserTagsService;
 
 @Controller
 @RequestMapping
 public class MyPageController {
-    
+
     @Autowired
     private UserTagsService userTagsService;
-    
+
+    @Autowired
+    private BusinessProfileService businessProfileService; // BusinessProfileService 주입
 
     @GetMapping("business/businessMypage")
-    public String businessMypage() {
+    public String businessMypage(HttpSession session, Model model) throws Exception {
+        // 세션에서 로그인한 사용자 정보 가져오기
+        Member currentUser = (Member) session.getAttribute("loginUser");
+        if (currentUser != null) {
+            int businessNo = currentUser.getBusinessNo();
+            // Business 객체 가져오기
+            Business business = businessProfileService.viewBusinessProfile(businessNo);
+            // 모델에 추가
+            model.addAttribute("business", business);
+        } else {
+            // 로그인하지 않은 경우 처리
+            return "redirect:/login"; // 로그인 페이지로 리다이렉트하거나 적절한 처리
+        }
         return "business/businessMypage";
     }
     
