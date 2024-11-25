a9fe8a6866d549f7417392d6a521b48c79dcd343
ajy3753 <ajy3753@gmail.com>
2024-11-25 16:55:59 +0900

    BusinessController 수정

--- src/main/java/com/cs/workdream/business/model/dao/BusinessDao.java
+++ src/main/java/com/cs/workdream/business/model/dao/BusinessDao.java
@@ -6,16 +6,14 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.cs.workdream.business.model.vo.Applicants;
+import com.cs.workdream.business.model.vo.ApplicantsStatus;
 
 @Repository
 public class BusinessDao {
 
 	// 지원자 현황 조회
-	public Applicants inquireAppStatus(SqlSessionTemplate sqlSession, int recruitmentNo) {
-		// DB 추가하고 다시 수정 + Applicants 대신 list 사용 검토
-		// 총 지원자 수 / 열람완료 수 / 평가완료 수 / 합격자 수 / 열람미완료 수 > status 조회로 int에 저장
-		// 지원 목록 > list 사용해서 저장 또는 객체 새로 생성
-		return null;
+	public ApplicantsStatus inquireAppsStatus(SqlSessionTemplate sqlSession, int recruitmentNo) {
+		return sqlSession.selectOne("businessMapper.inquireAppsStatus", recruitmentNo);
 	};
 	
 	// 지원자 목록 조회
